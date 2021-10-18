44b335d1be23b4ddd7170bba68333103541d844b
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-10-19 01:12:06 +0500

    Add CooperationEngFrom

---
+++ src/js/components/cooperation-eng-form.js
@@ -0,0 +1,48 @@
+import formService from './LendingService';
+import { post } from 'axios';
+
+const CooperationEngFrom = () => {
+  const initForm = (form) => {
+    const forms = form.querySelectorAll('.form-frame__item');
+    const formError = forms[0].querySelectorAll('.form-frame__error');
+    const showSuccessSubmit = () => {
+      hideForms();
+      forms[1].classList.add('active');
+    };
+
+    const hideForms = () => {
+      forms.forEach((form) => {
+        if (form.classList.contains('active')) {
+          form.classList.remove('active');
+        }
+      });
+    };
+
+    forms[0].addEventListener('submit', (e) => {
+      e.preventDefault();
+      const elForm = e?.currentTarget;
+      const data = formService.scrabbleInputs(elForm);
+      if (!data) return;
+      form.classList.add('loading');
+      formService.sendForm(data, formService.addLead)
+        .then((res) => {
+          console.log(res);
+          showSuccessSubmit();
+        })
+        .catch((err) => {
+          console.log(err);
+          formError.innerText = err?.message;
+        })
+        .finally(() => {
+          form.classList.remove('loading');
+        });
+    });
+  };
+
+  const forms = document.querySelectorAll('.js-cooperation-from');
+  forms.forEach((form) => {
+    initForm(form);
+  });
+};
+
+export default CooperationEngFrom;
--- src/pages/cooperation-eng/index.js
+++ src/pages/cooperation-eng/index.js
@@ -5,12 +5,13 @@ import '../../js/common';
 import Header from '../../js/components/header';
 import CooperationFrom from '../../js/components/cooperation-form';
 import LazyLoad from 'vanilla-lazyload';
+import CooperationEngFrom from '../../js/components/cooperation-eng-form';
 /* import components and lib */
 
 /* Your JS Code goes here */
 window.addEventListener('DOMContentLoaded', () => {
   new LazyLoad();
-  CooperationFrom();
+  CooperationEngFrom();
 });
 
 window.addEventListener('load', () => {
