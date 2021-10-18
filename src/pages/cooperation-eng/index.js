a794ab87ce3818874b2095e957d560753fda378d
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-10-18 15:11:37 +0500

    Add cooperation eng page

---
+++ src/pages/cooperation-eng/index.js
@@ -0,0 +1,19 @@
+/* import style */
+import './index.scss';
+/* import common js */
+import '../../js/common';
+import Header from '../../js/components/header';
+import CooperationFrom from '../../js/components/cooperation-form';
+import LazyLoad from 'vanilla-lazyload';
+/* import components and lib */
+
+/* Your JS Code goes here */
+window.addEventListener('DOMContentLoaded', () => {
+  new LazyLoad();
+  Header();
+  CooperationFrom();
+});
+
+window.addEventListener('load', () => {
+
+});
